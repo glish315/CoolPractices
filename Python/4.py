1029541346dddc9483e28a66d30acce6aab79565
glish315 <glish315@proton.me>
2025-01-14 16:13:50 +0100

    4

--- Python/4.py
+++ Python/4.py
@@ -1,16 +1,18 @@
 def is_valid_brackets(s):
 
-    x = "{[()]}"
+    brackets = ["(", ")", "{", "}", "[", "]"]
 
-    if x == s:
+    if all(x in s for x in brackets):
         print("True")
-    else: 
+    else:
         print("False")
+
+
+
 
 
 is_valid_brackets("()[]{}")
 is_valid_brackets("([]}")
-is_valid_brackets("{[()]}")
 
 
 
