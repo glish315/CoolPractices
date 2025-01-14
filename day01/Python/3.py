d042ba35dab7b7682087560a5b00559e6836e6a5
glish315 <glish315@proton.me>
2025-01-14 17:00:21 +0100

    day 01

---
+++ day01/Python/3.py
@@ -0,0 +1,16 @@
+def count_even_numbers(start, end):
+
+    list = []
+    good_end = end + 1
+    while start < good_end:
+        if start % 2 == 0 and start * 1 != 0:
+            list.append(start)
+        start += 1
+
+    count = len(list) 
+    print(count)
+
+
+
+    
+count_even_numbers(1, 20)
