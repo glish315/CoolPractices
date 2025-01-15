4d02af3383d30908c5363fc0575e9196c30dd9b8
glish315 <glish315@proton.me>
2025-01-15 08:49:59 +0100

    fix

--- day01/Python/5.py
+++ day01/Python/5.py
@@ -1,13 +1,13 @@
-def quick_sort(array):
-    if len(array) <= 1:
-        return array
+def quick_sort(output):
+    if len(output) <= 1:
+        return output
 
-    pivot = array[len(array) // 2]
+    pivot = output[len(output) // 2]
     left = []
     right = []
     equal = []
 
-    for number in array:
+    for number in output:
         if pivot > number:
             left.append(number)
         elif pivot < number:
@@ -18,9 +18,6 @@ def quick_sort(array):
     return quick_sort(left) + equal + quick_sort(right)
 
 
-
-
 array = [2, 6, 3, 4, 1, 5, 7]
-sortted = quick_sort(array)
-print(sortted)
-
+sort = quick_sort(array)
+print(sort)
